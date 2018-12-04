a4723bc98c4dbca3a201e492fa88217bc82e1a3c
Adam Rankin <adam.rankin@gmail.com>
2018-12-04 12:29:41 -0500

    Creating blank icon
    Removing polling/pushing mechanism, vtk update pipeline works as intended

--- VideoPassthrough/qSlicerVideoPassthroughModuleWidget.cxx
+++ VideoPassthrough/qSlicerVideoPassthroughModuleWidget.cxx
@@ -41,201 +41,12 @@
 // VTK includes
 #include <vtkImageData.h>
 #include <vtkTexture.h>
-#include <vtkTrivialProducer.h>
 
 // VTK OpenVR includes
 #include <vtkOpenVRRenderer.h>
 
-// Qt includes
-#include <QMutex>
-#include <QMutexLocker>
-#include <QRunnable>
-#include <QThreadPool>
-
 // OS includes
 #include <thread>
-
-//-----------------------------------------------------------------------------
-/// \ingroup Slicer_QtModules_AugmentedReality
-class qPollTextureUpdateThread : public QRunnable
-{
-public:
-  //-----------------------------------------------------------------------------
-  void run() override
-  {
-    while (true)
-    {
-      QMutexLocker(&this->ThreadMutex);
-
-      if (this->ThreadCancelRequested)
-      {
-        return;
-      }
-
-      if (this->LeftEyeNode == nullptr || this->RightEyeNode == nullptr ||
-          this->LeftEyeNode->GetImageData() == nullptr || this->RightEyeNode->GetImageData() == nullptr)
-      {
-        std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        continue;
-      }
-
-      // See if modified or image data modified, if new, update textures with new image data
-      if (this->LeftEyeNode->GetImageData()->GetMTime() > this->LeftEyeLastMTime || this->LeftEyeModified)
-      {
-        this->LeftEyeModified = false;
-        this->LeftEyeLastMTime = this->LeftEyeNode->GetImageData()->GetMTime();
-      }
-
-      if (this->RightEyeNode->GetImageData()->GetMTime() > this->RightEyeLastMTime || this->RightEyeModified)
-      {
-        this->RightEyeModified = false;
-        this->RightEyeLastMTime = this->RightEyeNode->GetImageData()->GetMTime();
-      }
-    }
-  }
-
-
-  //-----------------------------------------------------------------------------
-  qPollTextureUpdateThread::~qPollTextureUpdateThread()
-  {
-    this->LeftEyeTexture->Delete();
-    this->RightEyeTexture->Delete();
-  }
-
-  //-----------------------------------------------------------------------------
-  void cancelThread()
-  {
-    QMutexLocker(&this->ThreadMutex);
-    this->ThreadCancelRequested = true;
-  }
-
-  //-----------------------------------------------------------------------------
-  void setLeftEyeNode(vtkMRMLScalarVolumeNode* node)
-  {
-    QMutexLocker(&this->ThreadMutex);
-    this->LeftEyeNode = node;
-    this->LeftEyeModified = true;
-  }
-
-  //-----------------------------------------------------------------------------
-  void setRightEyeNode(vtkMRMLScalarVolumeNode* node)
-  {
-    QMutexLocker(&this->ThreadMutex);
-    this->RightEyeNode = node;
-    this->RightEyeModified = true;
-  }
-
-  qMRMLVirtualRealityView*  VRView = nullptr;
-
-  bool                      ThreadCancelRequested = false;
-  QMutex                    ThreadMutex;
-
-  vtkTexture*               LeftEyeTexture = vtkTexture::New();
-  vtkTexture*               RightEyeTexture = vtkTexture::New();
-  vtkMRMLScalarVolumeNode*  LeftEyeNode = nullptr;
-  vtkMRMLScalarVolumeNode*  RightEyeNode = nullptr;
-  vtkMTimeType              LeftEyeLastMTime = 0;
-  vtkMTimeType              RightEyeLastMTime = 0;
-  bool                      LeftEyeModified = false;
-  bool                      RightEyeModified = false;
-};
-
-class qTrivialProducerTextureUpdateThread : public QRunnable
-{
-public:
-  //-----------------------------------------------------------------------------
-  void run() override
-  {
-    while (true)
-    {
-      this->ThreadMutex.lock();
-      if (this->ThreadCancelRequested)
-      {
-        return;
-      }
-
-      if (this->LeftEyeNode == nullptr || this->RightEyeNode == nullptr ||
-          this->LeftEyeNode->GetImageData() == nullptr || this->RightEyeNode->GetImageData() == nullptr)
-      {
-        this->ThreadMutex.unlock();
-        std::this_thread::sleep_for(std::chrono::milliseconds(16));
-        continue;
-      }
-
-      this->ThreadMutex.unlock();
-    }
-  }
-
-  //-----------------------------------------------------------------------------
-  qTrivialProducerTextureUpdateThread::qTrivialProducerTextureUpdateThread()
-  {
-    this->LeftEyeTexture->SetInputConnection(this->LeftTrivialProducer->GetOutputPort());
-    this->RightEyeTexture->SetInputConnection(this->RightTrivialProducer->GetOutputPort());
-  }
-
-  //-----------------------------------------------------------------------------
-  qTrivialProducerTextureUpdateThread::~qTrivialProducerTextureUpdateThread()
-  {
-    this->LeftEyeTexture->Delete();
-    this->RightEyeTexture->Delete();
-  }
-
-  //-----------------------------------------------------------------------------
-  void cancelThread()
-  {
-    ThreadMutex.lock();
-    this->ThreadCancelRequested = true;
-    this->ThreadMutex.unlock();
-  }
-
-  //-----------------------------------------------------------------------------
-  void setLeftEyeNode(vtkMRMLScalarVolumeNode* node)
-  {
-    this->ThreadMutex.lock();
-
-    this->LeftEyeNode = node;
-    if(node->GetImageData() == nullptr)
-    {
-      return;
-    }
-    this->LeftTrivialProducer->SetInputDataObject(node->GetImageData());
-    this->LeftEyeModified = true;
-
-    this->ThreadMutex.unlock();
-  }
-
-  //-----------------------------------------------------------------------------
-  void setRightEyeNode(vtkMRMLScalarVolumeNode* node)
-  {
-    this->ThreadMutex.lock();
-
-    this->RightEyeNode = node;
-    if (node->GetImageData() == nullptr)
-    {
-      return;
-    }
-    this->RightTrivialProducer->SetInputDataObject(node->GetImageData());
-    this->RightEyeModified = true;
-
-    this->ThreadMutex.unlock();
-  }
-
-  qMRMLVirtualRealityView*  VRView = nullptr;
-
-  bool                      ThreadCancelRequested = false;
-  QMutex                    ThreadMutex;
-
-  vtkTexture*               LeftEyeTexture = vtkTexture::New();
-  vtkTexture*               RightEyeTexture = vtkTexture::New();
-  vtkMRMLScalarVolumeNode*  LeftEyeNode = nullptr;
-  vtkMRMLScalarVolumeNode*  RightEyeNode = nullptr;
-  vtkTrivialProducer*       LeftTrivialProducer = vtkTrivialProducer::New();
-  vtkTrivialProducer*       RightTrivialProducer = vtkTrivialProducer::New();
-  vtkMTimeType              LeftEyeLastMTime = 0;
-  vtkMTimeType              RightEyeLastMTime = 0;
-  bool                      LeftEyeModified = false;
-  bool                      RightEyeModified = false;
-};
 
 //-----------------------------------------------------------------------------
 /// \ingroup Slicer_QtModules_AugmentedReality
@@ -244,7 +55,17 @@ class qSlicerVideoPassthroughModuleWidgetPrivate: public Ui_qSlicerVideoPassthro
 public:
   qSlicerVideoPassthroughModuleWidgetPrivate();
 
-  qPollTextureUpdateThread TextureUpdateThread;
+  qMRMLVirtualRealityView* VRView;
+
+  vtkTexture* LeftEyeTexture = vtkTexture::New();
+  vtkTexture* RightEyeTexture = vtkTexture::New();
+
+public:
+  ~qSlicerVideoPassthroughModuleWidgetPrivate()
+  {
+    this->LeftEyeTexture->Delete();
+    this->RightEyeTexture->Delete();
+  }
 };
 
 //-----------------------------------------------------------------------------
@@ -266,8 +87,6 @@ qSlicerVideoPassthroughModuleWidget::qSlicerVideoPassthroughModuleWidget(QWidget
 {
   Q_D(qSlicerVideoPassthroughModuleWidget);
 
-  d->TextureUpdateThread.setAutoDelete(false);
-
   qSlicerVirtualRealityModule* module = dynamic_cast<qSlicerVirtualRealityModule*>(qSlicerApplication::application()->moduleManager()->module("virtualreality"));
   assert(module); // dependency system should have assured us this exists
 
@@ -286,12 +105,10 @@ qSlicerVideoPassthroughModuleWidget::qSlicerVideoPassthroughModuleWidget(QWidget
       view = dynamic_cast<qMRMLVirtualRealityView*>(item);
       assert(view);
 
-      d->TextureUpdateThread.VRView = view;
-      d->TextureUpdateThread.VRView->renderer()->SetTexturedBackground(true);
-      d->TextureUpdateThread.VRView->renderer()->SetLeftBackgroundTexture(d->TextureUpdateThread.LeftEyeTexture);
-      d->TextureUpdateThread.VRView->renderer()->SetRightBackgroundTexture(d->TextureUpdateThread.RightEyeTexture);
-
-      QThreadPool::globalInstance()->start(&d->TextureUpdateThread);
+      d->VRView = view;
+      d->VRView->renderer()->SetTexturedBackground(true);
+      d->VRView->renderer()->SetLeftBackgroundTexture(d->LeftEyeTexture);
+      d->VRView->renderer()->SetRightBackgroundTexture(d->RightEyeTexture);
     }
   }
 }
@@ -301,7 +118,6 @@ qSlicerVideoPassthroughModuleWidget::~qSlicerVideoPassthroughModuleWidget()
 {
   Q_D(qSlicerVideoPassthroughModuleWidget);
 
-  d->TextureUpdateThread.cancelThread();
 }
 
 //----------------------------------------------------------------------------
@@ -309,9 +125,7 @@ void qSlicerVideoPassthroughModuleWidget::onLeftEyeNodeChanged(vtkMRMLScalarVolu
 {
   Q_D(qSlicerVideoPassthroughModuleWidget);
 
-  d->TextureUpdateThread.ThreadMutex.lock();
-  d->TextureUpdateThread.setLeftEyeNode(node);
-  d->TextureUpdateThread.ThreadMutex.unlock();
+  d->LeftEyeTexture->SetInputDataObject(node->GetImageData());
 }
 
 //----------------------------------------------------------------------------
@@ -319,9 +133,7 @@ void qSlicerVideoPassthroughModuleWidget::onRightEyeNodeChanged(vtkMRMLScalarVol
 {
   Q_D(qSlicerVideoPassthroughModuleWidget);
 
-  d->TextureUpdateThread.ThreadMutex.lock();
-  d->TextureUpdateThread.setRightEyeNode(node);
-  d->TextureUpdateThread.ThreadMutex.unlock();
+  d->RightEyeTexture->SetInputDataObject(node->GetImageData());
 }
 
 //-----------------------------------------------------------------------------
